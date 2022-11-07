ad2e7c84e1286de24ad440ef35dc229158e1c6f7
Alvaro Scelza <alvaroscelza@gmail.com>
2022-11-07 20:52:51 -0300

    deleted unused files

--- applications/core/tests/factories/test_data_factories.py
+++
@@ -1,4 +0,0 @@
-class TestDataFactory:
-    @staticmethod
-    def load_default_test_data():
-        return None
--- applications/core/tests/factories/unique_name_factory_mixin.py
+++
@@ -1,9 +0,0 @@
-import factory
-from factory.django import DjangoModelFactory
-
-
-class UniqueNameFactoryMixin(DjangoModelFactory):
-    class Meta:
-        django_get_or_create = ['name']
-
-    name = factory.Faker('pystr')
