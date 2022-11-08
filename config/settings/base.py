1a21567b07f817122a989e140452de1ab96fddf2
Alvaro Scelza <alvaroscelza@gmail.com>
2022-11-07 22:28:35 -0300

    removed media folder and url

--- config/settings/base.py
+++ config/settings/base.py
@@ -61,6 +61,4 @@ WSGI_APPLICATION = 'config.wsgi.application'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_storage')
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # endregion
--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,4 @@
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import RedirectView
@@ -18,8 +17,4 @@ urlpatterns += [
     path('', views.get_posts, name='posts'),
     path('login/', RedirectView.as_view(pattern_name='posts')),
 ]
-# endregion
-
-# region Media configuration
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # endregion
